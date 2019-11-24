042292c95ea697d47971b80c56b180a06ae6f9a7
zedr0n <zedr0n@protonmail.com>
2019-11-24 20:36:00 +0000

    add some kingdom mechanics hacks

--- CallOfTheWild/GlobalMap.cs
+++ CallOfTheWild/GlobalMap.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Harmony12;
+using Kingmaker.Kingdom.Blueprints;
+using Kingmaker.Kingdom.Tasks;
+using UnityEngine;
 
 namespace CallOfTheWild.GlobalMap
 {
@@ -16,6 +20,49 @@ namespace CallOfTheWild.GlobalMap
             {
                 __result = 0;
                 return false;
+            }
+        }*/
+        [HarmonyPatch(typeof(KingdomEvent), "CalculateRulerTime")]
+        static class KingdomEvent_CalculateRulerTime_Patch
+        {
+            static void Postfix(KingdomEvent __instance, ref int __result)
+            {
+                try
+                {
+                    if (__result > 14)
+                        __result /= 2;
+                    else if (__result > 7)
+                        __result = 7;
+                }
+                catch (Exception ex)
+                {
+                    Main.DebugError(ex);
+                }
+            }
+        }
+
+
+
+        /*[HarmonyPatch(typeof(KingdomTaskEvent), "SkipPlayerTime", MethodType.Getter)]
+        static class KingdomTaskEvent_SkipPlayerTime_Patch
+        {
+            static void Postfix(ref int __result)
+            {
+                try
+                {
+                    if (!Main.enabled) return;
+                    if (__result < 1) return;
+                    if(__result > 14)
+                        __result /= 2;
+                    else if (__result > 7)
+                        __result = 7;
+                    
+                    __result = __result < 1 ? 1 : __result;
+                }
+                catch (Exception ex)
+                {
+                    Main.DebugError(ex);
+                }
             }
         }*/
 }
